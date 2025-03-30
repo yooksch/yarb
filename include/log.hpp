9374be06754fb042b5c6015ea44d0600d3057681
yooksch <yooksch@pm.me>
2025-03-30 13:52:45 +0200

    Remove fmt as a dependency

--- include/log.hpp
+++ include/log.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <cassert>
-#include <fmt/format.h>
 #include <fstream>
 #include <iostream>
 #include <format>
 #include <chrono>
+
+template <typename T>
+concept Formattable = requires (T value) {
+    std::format("", value);
+};
 
 class Log {
 public:
@@ -14,25 +18,25 @@ public:
     static const int LEVEL_ERROR = 2;
     static const int LEVEL_DEBUG = 3;
 
-    template<typename... T>
-    static inline void Info(const char* ident, fmt::format_string<T...> fmt, T&&... args) {
+    template<Formattable... T>
+    static inline void Info(const char* ident, std::format_string<T...> fmt, T&&... args) {
         Println("INFO", ident, fmt, std::forward<T&&...>(args)...);
     }
 
-    template<typename... T>
-    static inline void Warning(const char* ident, fmt::format_string<T...> fmt, T&&... args) {
+    template<Formattable... T>
+    static inline void Warning(const char* ident, std::format_string<T...> fmt, T&&... args) {
         if (log_level < Log::LEVEL_WARNING) return;
         Println("WARNING", ident, fmt, std::forward<T&&...>(args)...);
     }
 
-    template<typename... T>
-    static inline void Error(const char* ident, fmt::format_string<T...> fmt, T&&... args) {
+    template<Formattable... T>
+    static inline void Error(const char* ident, std::format_string<T...> fmt, T&&... args) {
         if (log_level < Log::LEVEL_ERROR) return;
         Println("ERROR", ident, fmt, std::forward<T&&...>(args)...);
     }
 
-    template<typename... T>
-    static inline void Debug(const char* ident, fmt::format_string<T...> fmt, T&&... args) {
+    template<Formattable... T>
+    static inline void Debug(const char* ident, std::format_string<T...> fmt, T&&... args) {
         if (log_level < Log::LEVEL_DEBUG) return;
         Println("DEBUG", ident, fmt, std::forward<T&&...>(args)...);
     }
@@ -51,12 +55,12 @@ private:
     static std::unique_ptr<std::ofstream> log_file;
     static int log_level;
 
-    template<typename... T>
-    static void Println(const char* mode, const char* ident, fmt::format_string<T...> fmt, T&&... args) {
+    template<Formattable... T>
+    static void Println(const char* mode, const char* ident, std::format_string<T...> fmt, T&&... args) {
         std::string formatted_time = std::format("{:%T}", std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()));
 
-        std::string msg = fmt::format(fmt, std::forward<T&&...>(args)...);
-        msg = fmt::format("[{}] [{}] [{}] {}", formatted_time, ident, mode, msg);
+        std::string msg = std::format(fmt, std::forward<T&&...>(args)...);
+        msg = std::format("[{}] [{}] [{}] {}", formatted_time, ident, mode, msg);
     
         std::cout << msg << "\n";
         if (log_file != nullptr) {
