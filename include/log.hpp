580cd4db82b2709b90b271742952ec3410cf277a
yooksch <yooksch@pm.me>
2025-04-07 17:28:27 +0200

    Improve log

--- include/log.hpp
+++ include/log.hpp
@@ -19,26 +19,26 @@ public:
     static const int LEVEL_DEBUG = 3;
 
     template<Formattable... T>
-    static inline void Info(const char* ident, std::format_string<T...> fmt, T&&... args) {
-        Println("INFO", ident, fmt, std::forward<T&&...>(args)...);
+    static void Info(const char* ident, std::format_string<T...> fmt, T&&... args) {
+        Println("INFO", ident, std::format(fmt, std::forward<T>(args)...));
     }
 
     template<Formattable... T>
-    static inline void Warning(const char* ident, std::format_string<T...> fmt, T&&... args) {
+    static void Warning(const char* ident, std::format_string<T...> fmt, T&&... args) {
         if (log_level < Log::LEVEL_WARNING) return;
-        Println("WARNING", ident, fmt, std::forward<T&&...>(args)...);
+        Println("WARNING", ident, std::format(fmt, std::forward<T>(args)...));
     }
 
     template<Formattable... T>
-    static inline void Error(const char* ident, std::format_string<T...> fmt, T&&... args) {
+    static void Error(const char* ident, std::format_string<T...> fmt, T&&... args) {
         if (log_level < Log::LEVEL_ERROR) return;
-        Println("ERROR", ident, fmt, std::forward<T&&...>(args)...);
+        Println("ERROR", ident, std::format(fmt, std::forward<T>(args)...));
     }
 
     template<Formattable... T>
-    static inline void Debug(const char* ident, std::format_string<T...> fmt, T&&... args) {
+    static void Debug(const char* ident, std::format_string<T...> fmt, T&&... args) {
         if (log_level < Log::LEVEL_DEBUG) return;
-        Println("DEBUG", ident, fmt, std::forward<T&&...>(args)...);
+        Println("DEBUG", ident, std::format(fmt, std::forward<T>(args)...));
     }
 
     static void SetLevel(int level) {
@@ -56,10 +56,9 @@ private:
     static int log_level;
 
     template<Formattable... T>
-    static void Println(const char* mode, const char* ident, std::format_string<T...> fmt, T&&... args) {
+    static void Println(const char* mode, const char* ident, std::string msg) {
         std::string formatted_time = std::format("{:%T}", std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()));
 
-        std::string msg = std::format(fmt, std::forward<T&&...>(args)...);
         msg = std::format("[{}] [{}] [{}] {}", formatted_time, ident, mode, msg);
     
         std::cout << msg << "\n";
